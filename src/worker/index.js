de5cadfa427560603455334729eaf6b01b672584
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-08-17 18:59:16 +0900

    implement batch

--- src/worker/index.js
+++ src/worker/index.js
@@ -1,26 +1,46 @@
-import throng from 'throng';
-import jackrabbit from 'jackrabbit';
-import logger from 'logfmt';
+const puppeteer = require('puppeteer');
 
-throng({ workers: 1 }, () => {
-  const shutdown = () => {
-    logger.log({ type: 'info', msg: 'shutting down' });
-    process.exit();
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setRequestInterceptionEnabled(true);
+  page.on('request', (request) => {
+    if (/\.(png|jpg|jpeg|gif|webp)$/.test(request.url)) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
+  const baseURL = 'https://www.happyon.jp';
+  const genreURL = `${baseURL}/tiles/genres/animation`;
+  const loginURL = `${baseURL}/account/login`;
+  const profileGateURL = `${loginURL}/profiles/select`;
+  await page.goto(genreURL, { waitUntil: 'networkidle' });
+  if (await page.url() === loginURL) {
+    await page.focus('#form_account_email');
+    await page.type(process.env.SVOD_ACCOUNTS_HULU_ID);
+    await page.focus('#form_account_password');
+    await page.type(process.env.SVOD_ACCOUNTS_HULU_PW);
+    await page.click('#new_form_account button[type=submit]');
   }
-  process.on('SIGTERM', shutdown);
-  process.on('SIGINT', shutdown);
+  if (await page.url() === profileGateURL) {
+    const profiles = await page.evaluate(selector => (
+      [...document.querySelectorAll(selector)].map(n => n.textContent.trim())
+    ), '.vod-mod-profile__name');
+    const i = profiles.findIndex(
+      profile => profile === process.env.SVOD_ACCOUNTS_HULU_NAME
+    );
+    await page.click(
+      `.vod-mod-profile-list__item:nth-child(${i + 1}) input[type=image]`
+    );
+  }
+  await sleep(2000);
+})().then(() => process.exit()).catch(error => console.log(error));
 
-  const onMessage = ({ service }, ack) => {
-    logger.log({
-      type: 'info', msg: 'on message', queue: 'jobs.crawl.list', service,
-    });
-    ack();
-  };
-
-  const exchange = jackrabbit(process.env.CLOUDAMQP_URL).default();
-  exchange
-    .queue({ name: 'jobs.crawl.list', durable: true })
-    .consume(onMessage);
-
-  logger.log({ type: 'info', msg: 'worker ready' });
+// error handling
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
 });
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
