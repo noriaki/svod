1f1d74549f3042fb5d6c08fee7e6bf421af97d59
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-08-18 00:13:41 +0900

    enhance add `visit()` that login and goto

---
+++ batch/hulu/crawler.js
@@ -0,0 +1,39 @@
+const host = 'www.happyon.jp';
+const baseURL = `https://${host}`;
+const loginURL = `${baseURL}/account/login`;
+const profileGateURL = `${loginURL}/profiles/select`;
+// const genreBaseURL = `${baseURL}/tiles/genres`;
+
+const visit = async (page, url, options) => {
+  if (url !== await page.url()) {
+    await page.goto(url, options);
+  } else {
+    return;
+  }
+  if (await page.url() === loginURL) {
+    await page.focus('#form_account_email');
+    await page.type(process.env.SVOD_ACCOUNTS_HULU_ID);
+    await page.focus('#form_account_password');
+    await page.type(process.env.SVOD_ACCOUNTS_HULU_PW);
+    await page.click('#new_form_account button[type=submit]');
+  }
+  if (await page.url() === profileGateURL) {
+    const profiles = await page.evaluate(selector => (
+      [...document.querySelectorAll(selector)].map(n => n.textContent.trim())
+    ), '.vod-mod-profile__name');
+    const i = profiles.findIndex(
+      profile => profile === process.env.SVOD_ACCOUNTS_HULU_NAME
+    );
+    await page.click(
+      `.vod-mod-profile-list__item:nth-child(${i + 1}) input[type=image]`
+    );
+  }
+  if (url !== await page.url()) {
+    await page.goto(url, options);
+  }
+};
+module.visit = visit;
+
+module.exports = {
+  visit,
+};
--- batch/hulu/index.js
+++ batch/hulu/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { visit } = require('./crawler');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -13,27 +14,7 @@ const puppeteer = require('puppeteer');
   });
   const baseURL = 'https://www.happyon.jp';
   const genreURL = `${baseURL}/tiles/genres/animation`;
-  const loginURL = `${baseURL}/account/login`;
-  const profileGateURL = `${loginURL}/profiles/select`;
-  await page.goto(genreURL, { waitUntil: 'networkidle' });
-  if (await page.url() === loginURL) {
-    await page.focus('#form_account_email');
-    await page.type(process.env.SVOD_ACCOUNTS_HULU_ID);
-    await page.focus('#form_account_password');
-    await page.type(process.env.SVOD_ACCOUNTS_HULU_PW);
-    await page.click('#new_form_account button[type=submit]');
-  }
-  if (await page.url() === profileGateURL) {
-    const profiles = await page.evaluate(selector => (
-      [...document.querySelectorAll(selector)].map(n => n.textContent.trim())
-    ), '.vod-mod-profile__name');
-    const i = profiles.findIndex(
-      profile => profile === process.env.SVOD_ACCOUNTS_HULU_NAME
-    );
-    await page.click(
-      `.vod-mod-profile-list__item:nth-child(${i + 1}) input[type=image]`
-    );
-  }
+  await visit(page, genreURL, { waitUntil: 'networkidle' });
   await sleep(2000);
 })().then(() => process.exit()).catch(error => console.log(error));
 
