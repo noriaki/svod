069f39b8aa8e2568bcebf4beb5cc3df805802e47
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-08-18 09:50:54 +0900

    enhance separate methods

--- batch/hulu/crawler.js
+++ batch/hulu/crawler.js
@@ -6,42 +6,56 @@ const loginURL = `${baseURL}/account/login`;
 const profileGateURL = `${loginURL}/profiles/select`;
 // const genreBaseURL = `${baseURL}/tiles/genres`;
 
+const gotoAndWaitLoad = async (page, url, options = {}) => (
+  await page.goto(url, { waitUntil: 'networkidle', ...options })
+);
+module.gotoAndWaitLoad = gotoAndWaitLoad;
+
+const login = async (page) => {
+  if (await page.url() !== loginURL) {
+    await gotoAndWaitLoad(page, loginURL);
+  }
+  await page.focus('#form_account_email');
+  await page.type(process.env.SVOD_ACCOUNTS_HULU_ID);
+  await page.focus('#form_account_password');
+  await page.type(process.env.SVOD_ACCOUNTS_HULU_PW);
+  await page.click('#new_form_account button[type=submit]');
+};
+module.login = login;
+
+const selectProfile = async (page) => {
+  const profiles = await page.evaluate(selector => (
+    [...document.querySelectorAll(selector)].map(n => n.textContent.trim())
+  ), '.vod-mod-profile__name');
+  const i = profiles.findIndex(
+    profile => profile === process.env.SVOD_ACCOUNTS_HULU_NAME
+  );
+  await page.click(
+    `.vod-mod-profile-list__item:nth-child(${i + 1}) input[type=image]`
+  );
+}
+module.selectProfile = selectProfile;
+
 const visit = async (page, url, options) => {
   if (url !== await page.url()) {
-    await page.goto(url, options);
+    await gotoAndWaitLoad(page, url, options);
   } else {
     return;
   }
-  if (await page.url() === loginURL) {
-    await page.focus('#form_account_email');
-    await page.type(process.env.SVOD_ACCOUNTS_HULU_ID);
-    await page.focus('#form_account_password');
-    await page.type(process.env.SVOD_ACCOUNTS_HULU_PW);
-    await page.click('#new_form_account button[type=submit]');
-  }
-  if (await page.url() === profileGateURL) {
-    const profiles = await page.evaluate(selector => (
-      [...document.querySelectorAll(selector)].map(n => n.textContent.trim())
-    ), '.vod-mod-profile__name');
-    const i = profiles.findIndex(
-      profile => profile === process.env.SVOD_ACCOUNTS_HULU_NAME
-    );
-    await page.click(
-      `.vod-mod-profile-list__item:nth-child(${i + 1}) input[type=image]`
-    );
-  }
+  if (await page.url() === loginURL) { await login(page); }
+  if (await page.url() === profileGateURL) { await selectProfile(page); }
   if (url !== await page.url()) {
-    await page.goto(url, options);
+    await gotoAndWaitLoad(page, url, options);
   }
 };
 module.visit = visit;
 
 const enhance = page => ({
+  gotoAndWaitLoad: partial(gotoAndWaitLoad, page),
+  login: partial(login, page),
+  selectProfile: partial(selectProfile, page),
   visit: partial(visit, page),
 });
 module.enhance = enhance;
 
-module.exports = {
-  visit,
-  enhance,
-};
+module.exports = enhance;
--- batch/hulu/index.js
+++ batch/hulu/index.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const { enhance } = require('./crawler');
+const enhance = require('./crawler');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -15,7 +15,7 @@ const { enhance } = require('./crawler');
   const baseURL = 'https://www.happyon.jp';
   const genreURL = `${baseURL}/tiles/genres/animation`;
   const { visit } = enhance(page);
-  await visit(genreURL, { waitUntil: 'networkidle' });
+  await visit(genreURL);
   await sleep(2000);
 })().then(() => process.exit()).catch(error => console.log(error));
 
@@ -26,3 +26,34 @@ process.on('unhandledRejection', (err) => {
 });
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+const genres = [
+  ["science_fiction", "SF"],
+  ["human_drama", "ヒューマンドラマ"],
+  ["animation", "アニメ"],
+  ["action", "アクション"],
+  ["comedy", "コメディ"],
+  ["military_and_war", "戦争"],
+  ["panic_and_disaster", "パニック"],
+  ["suspense_mystery", "サスペンス／ミステリー"],
+  ["adventure", "アドベンチャー"],
+  ["fantasy", "ファンタジー"],
+  ["westerns", "西部劇"],
+  ["kids", "キッズ"],
+  ["horror", "ホラー"],
+  ["special_effects", "特撮"],
+  ["coming_of_age", "青春"],
+  ["romance", "恋愛"],
+  ["japanese_period_drama", "時代劇"],
+  ["music_live", "音楽／ライブ"],
+  ["food", "料理"],
+  ["sport", "スポーツ"],
+  ["historical_drama", "史劇"],
+  ["family", "ファミリー"],
+  ["variety", "バラエティ"],
+  ["documentary", "ドキュメンタリー"],
+  ["other", "その他"],
+  ["travel_hobby", "旅行／趣味"],
+  ["reality_shows", "リアリティーショー"],
+  ["news", "ニュース"],
+];
